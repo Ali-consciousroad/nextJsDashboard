960c43f2de223d9cac659419bf6022aa589547ad
ali-consciousroad <ali_dindar@live.be>
2024-06-18 10:57:43 +0200

    Add secondary font + logo

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,10 @@
 // Adding a primary font
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
+
+// Adding a secondary font
+export const lusitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
