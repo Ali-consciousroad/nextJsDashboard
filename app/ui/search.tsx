6d9fcbf6f8db2eded371405f080991495260529f
ali-consciousroad <ali_dindar@live.be>
2024-07-22 13:07:09 +0200

    Search functionality: Update the URL with the search params and keep the URL and input in sync

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    /* URLSearchParams is a web API that provides utility methods 
+    for simplified URL query parameters manipulation */
+    const params = new URLSearchParams(searchParams);
+    // If the input is empty, then delete it
+    if (term) {
+      params.set('query', term);}
+    else {
+      params.delete('query');
+    }
+    // ${pathname} is the current path, in this case, "/dashboard/invoices"
+    // Updates the URL with the user's search data
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -13,12 +29,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       {/* By adding an onChange listener to the <input> element, 
           onChange will invoke handleSearch whenever the input value changes. */}
+      {/* The defaultValue for the input by reading from searchParams will ensur the input field is in sync with the URL and will be populated when sharing */}     
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
