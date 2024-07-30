004ed6a9768d43dc06f43dd6814b2297939fafdf
ali-consciousroad <ali_dindar@live.be>
2024-07-30 17:16:57 +0200

    Use Zod validation library for type validation and coercion

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,15 +2,26 @@
 /* Server Actions could have been written directly inside Server Components by adding "use server" inside the action.
 For this course, we preferred to keep them all organized in a separate file. */
 'use server';
+// Import Zod, a TypeScript-first validation library 
+import { z } from 'zod';
+
+/* The schema that matches the form of our object will validate the formData 
+before saving it to the DB. */
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(), // The amount field is set to coerce (change) from a string to a number while also validating its type
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 // Extract the data from formData
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
-    // Test it out:
-    // Display the form in the terminal after submitting it
-    console.log(rawFormData);
+    });
 }
