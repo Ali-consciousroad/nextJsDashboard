979adffcc3df6248d3a83238649cb0e3a502a111
ali-consciousroad <ali_dindar@live.be>
2025-01-15 20:07:37 +0100

    Use promise to handle the asynchronous cases ensuring that the query parameters are fully resolved before being used in the component to handle more complex scenarios

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,14 +7,13 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
  
-export default async function Page({
-  searchParams,
-}:{
-  searchParams?: {
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
     page?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
@@ -36,6 +35,7 @@ export default async function Page({
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense> 
+      {/* The <Pagingation/> component allows users to navigate between pages of invoices. */}
       <div className="mt-5 flex w-full justify-center">
         {/* Pass the totalPages prop to the <Pagination/> component */}
         <Pagination totalPages={totalPages} />
