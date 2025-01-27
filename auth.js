a8f843b130a9f62636a8ac81b0d4fc874e8a1a2e
ali-consciousroad <ali_dindar@live.be>
2025-01-27 13:28:10 +0000

    Validate the email and password using zod

--- auth.js
+++ auth.js
@@ -1,7 +1,19 @@
 // Spread the authConfig object
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+// We use zod to validate the email and password before checking if the user exists in the DB.
+import { z } from 'zod';
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+                },
+            }),  
+        ],
 });
