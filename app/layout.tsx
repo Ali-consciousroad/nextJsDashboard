2b497a7885e84881d3692baf5da96f270eabf0dc
ali-consciousroad <ali_dindar@live.be>
2025-01-28 13:00:39 +0000

    Add page title and descriptions using the metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 // Shared root layout (required)
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+// Any metadata in layout.js will be inherited by all papges that use it.
+// Next.js will automatically add the title and metadata to your application.
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The  official Next.js course dashboard built with App router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
